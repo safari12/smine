39280d17a8d4c1f06adfd94286a05a7ddb90fc5f
safari <rsafari.s@gmail.com>
2018-06-16 23:31:50 -0500

    added mock testing for config parser module

---
+++ test/config-mock.js
@@ -0,0 +1,12 @@
+module.exports = {
+  discovery: {
+    number_of_miners: 100,
+    hostname: {
+      prefix: 's-m-'
+    },
+    api: {
+      port: 6969,
+      endpoint: '/api.json'
+    }
+  }
+}
