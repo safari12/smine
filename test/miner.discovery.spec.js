6439145fad797055859c002a49f14af5bc03980c
safari <rsafari.s@gmail.com>
2018-06-18 00:15:40 -0500

    tested getting online miner stats

--- test/miner.discovery.spec.js
+++ test/miner.discovery.spec.js
@@ -1,5 +1,8 @@
+const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+const request = require('request-promise')
 
+const Miner = require('../lib/miner/model')
 const MinerDiscovery = require('../lib/miner/discovery')
 
 const chai = require('chai')
@@ -27,5 +30,46 @@ describe('MinerDiscovery', () => {
     minerDiscovery.numberOfMiners.should.equal(data.numberOfMiners)
     minerDiscovery.hostname.should.deep.equal(data.hostname)
     minerDiscovery.api.should.deep.equal(data.api)
+  })
+  describe('getStats', () => {
+    it('should return online miners with expected data', async () => {
+      const expectedBody = { message: 'test' }
+      const expectedNumberOfMiners = 5
+      const expectedHostname = { prefix: 's-m-'}
+      const expectedApi = { port: 6969, endpoint: '/api.json' }
+      const expectedSoftware = 'xmr-stak'
+
+      const stub = sinon.stub(request, 'get')
+      stub.returns(
+        new Promise((resolve) => {
+          resolve(expectedBody)
+        })
+      )
+
+      const minerDiscovery = new MinerDiscovery(
+        expectedNumberOfMiners,
+        expectedHostname,
+        expectedApi
+      )
+
+      const stats = await minerDiscovery.getStats()
+
+      stats.online.should.have.lengthOf(expectedNumberOfMiners)
+      stats.offline.should.have.lengthOf(0)
+
+      for (let i = 0; i < expectedNumberOfMiners; i++) {
+        const minerId = Miner.getId(i + 1)
+        const expectedUri = `http://${expectedHostname.prefix + minerId}:${expectedApi.port}${expectedApi.endpoint}`
+        const miner = stats.online[i]
+
+        miner.data.should.equal(expectedBody)
+        miner.name.should.equal(expectedHostname.prefix + minerId)
+        miner.software.should.equal(expectedSoftware)
+
+        stub.should.have.been.calledWith(expectedUri)
+      }
+
+      stub.restore()
+    })
   })
 })
