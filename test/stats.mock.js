64df1ce24bb4908f6b2f4dcb1e8306a4744b80db
safari <rsafari.s@gmail.com>
2018-07-29 03:05:57 -0500

    refactored test cases for notification module

---
+++ test/stats.mock.js
@@ -0,0 +1,25 @@
+module.exports = {
+  rigs: {
+    currently: {
+      online: [{
+        name: 's-m-01',
+        hashrate: 4000
+      }, {
+        name: 's-m-02',
+        hashrate: 4000
+      }],
+      offline: [{
+        name: 's-m-03',
+        hashrate: 3000
+      }, {
+        name: 's-m-04',
+        hashrate: 4000
+      }]
+    },
+    new: {
+      online: [],
+      offline: []
+    },
+    fixed: []
+  }
+}
