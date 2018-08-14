db6912b292b93bb4687849be4521213f56d956b7
safari <rsafari.s@gmail.com>
2018-08-14 02:10:44 -0500

    added navbar

--- ui/src/app/layout/layout.module.ts
+++ ui/src/app/layout/layout.module.ts
@@ -4,11 +4,18 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 
+import { NavbarComponent } from '../navbar/navbar.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FooterComponent, HeaderComponent, ContentComponent],
+  declarations: [
+    FooterComponent, 
+    HeaderComponent, 
+    ContentComponent,
+    NavbarComponent
+  ],
   exports: [ContentComponent]
 })
 export class LayoutModule { }
