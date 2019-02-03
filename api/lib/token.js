4919f09ca2594b62f8f7f8f15814501ebcfce6ad
safari <rsafari.s@gmail.com>
2019-02-03 13:13:17 -0600

    implemented token check for middleware

---
+++ api/lib/token.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken')
+
+const config = require('./config')
+
+class Token {
+  static async check(req, res, next) {
+    const token = this.getToken(req)
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'No token provided'
+      })
+    }
+
+    try {
+      await jwt.verify(token, config.api.secret)
+      next()
+    } catch (error) {
+      res.status(401).json({
+        success: false,
+        message: 'Failed to authenticate token'
+      })
+    }
+  }
+
+  static async checkAdmin(req, res, next) {
+    const token = this.getToken(req)
+    const user = await jwt.decode(token)
+
+    if (!user.admin) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorize access'
+      })
+    }
+
+    next()
+  }
+
+  static getToken(req) {
+    return req.query.token || req.body.token || req.headers['x-access-token']
+  }
+}
+
+module.exports = Token
