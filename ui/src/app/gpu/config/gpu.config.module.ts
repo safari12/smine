4548366bf25590b091f61f5c137945399902ae1b
safari <rsafari.s@gmail.com>
2019-02-25 22:22:02 -0600

    added modal form for gpu config

--- ui/src/app/gpu/config/gpu.config.module.ts
+++ ui/src/app/gpu/config/gpu.config.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common'
 import { GpuConfigTableComponent } from './table/gpu.config.table.component'
 import { GpuConfigTableHeaderComponent } from './table/header/gpu.config.table.header.component'
 import { GpuConfigModalComponent } from './modal/gpu.config.modal.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { GpuConfigModalComponent } from './modal/gpu.config.modal.component'
     GpuConfigModalComponent
   ],
   entryComponents: [GpuConfigModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [GpuConfigTableComponent, GpuConfigTableHeaderComponent]
 })
 export class GpuConfigModule {}
--- ui/src/app/gpu/config/modal/gpu.config.modal.component.ts
+++ ui/src/app/gpu/config/modal/gpu.config.modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
+import { FormBuilder, Validators, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-gpu-config-modal',
@@ -9,5 +10,32 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
 export class GpuConfigModalComponent {
   @Input() config: any
 
-  constructor(public activeModal: NgbActiveModal) {}
+  configForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    cardCount: ['', [Validators.required, Validators.min(1)]],
+    powerLimit: ['', [Validators.required, Validators.min(75)]],
+    api: this.fb.group({
+      endpoint: ['', Validators.required],
+      port: ['', Validators.required],
+      retries: [''],
+      timeout: ['']
+    })
+  })
+  submitted = false
+
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder) {}
+
+  get cf() {
+    return this.configForm.controls
+  }
+
+  onSubmit() {
+    this.submitted = true
+
+    if (this.configForm.invalid) {
+      return
+    }
+
+    this.activeModal.close('Close click')
+  }
 }
