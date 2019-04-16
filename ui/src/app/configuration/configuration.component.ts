08be85b10d7d046c8ff0fe3eac855fa58efe2611
safari <rsafari.s@gmail.com>
2019-04-15 19:08:28 -0500

    code clean up

--- ui/src/app/configuration/configuration.component.ts
+++ ui/src/app/configuration/configuration.component.ts
@@ -1,76 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
-import { MinerConfigModalComponent } from '../miner/config/modal/miner.config.modal.component'
-import { MinerConfig } from '../miner/config/miner.config'
-import MinerConfigService from '../miner/config/miner.config.service'
-import MinerService from '../miner/miner.service'
-import { delay } from 'rxjs/operators'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
   styleUrls: ['./configuration.component.css']
 })
-export class ConfigurationComponent implements OnInit {
-  minerConfigs: MinerConfig[]
-  miners: string[]
-
-  constructor(
-    private modalService: NgbModal,
-    private minerService: MinerService,
-    private minerConfigService: MinerConfigService
-  ) {}
-
-  ngOnInit() {
-    this.minerConfigService.modelSource.subscribe(minerConfigs => {
-      this.minerConfigs = minerConfigs
-    })
-    this.minerService.getSupported().subscribe(miners => {
-      this.miners = miners
-    })
-    this.minerConfigService.readAll()
-  }
-
-  openMinerModal() {
-    const modal = this.modalService.open(MinerConfigModalComponent, {
-      size: 'lg',
-      centered: true
-    })
-    modal.componentInstance.miners = this.miners
-
-    return modal
-  }
-
-  createMinerConfig() {
-    const modal = this.openMinerModal()
-    modal.componentInstance.onCreate.subscribe(config => {
-      modal.componentInstance.loading = true
-      this.minerConfigService
-        .create(config)
-        .pipe(delay(1000))
-        .subscribe(() => {
-          modal.componentInstance.loading = false
-          modal.close()
-        })
-    })
-  }
-
-  editMinerConfig(config) {
-    const modal = this.openMinerModal()
-    modal.componentInstance.config = config
-    modal.componentInstance.onUpdate.subscribe(config => {
-      modal.componentInstance.loading = true
-      this.minerConfigService
-        .update(config)
-        .pipe(delay(1000))
-        .subscribe(() => {
-          modal.componentInstance.loading = false
-          modal.close()
-        })
-    })
-  }
-
-  deleteMinerConfig(config) {
-    this.minerConfigService.remove(config)
-  }
-}
+export class ConfigurationComponent {}
--- ui/src/app/miner/config/miner.config.module.ts
+++ ui/src/app/miner/config/miner.config.module.ts
@@ -5,16 +5,22 @@ import { MinerConfigTableComponent } from './table/miner.config.table.component'
 import MinerConfigService from './miner.config.service'
 import { MinerConfigModalComponent } from './modal/miner.config.modal.component'
 import { ReactiveFormsModule } from '@angular/forms'
+import { MinerConfigComponent } from './miner.config.component'
 
 @NgModule({
   declarations: [
     MinerConfigTableHeaderComponent,
     MinerConfigTableComponent,
-    MinerConfigModalComponent
+    MinerConfigModalComponent,
+    MinerConfigComponent
   ],
   imports: [CommonModule, ReactiveFormsModule],
   entryComponents: [MinerConfigModalComponent],
   providers: [MinerConfigService],
-  exports: [MinerConfigTableComponent, MinerConfigTableHeaderComponent]
+  exports: [
+    MinerConfigTableComponent,
+    MinerConfigTableHeaderComponent,
+    MinerConfigComponent
+  ]
 })
 export class MinerConfigModule {}
