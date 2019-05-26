6310b05e5f3644cb677f24d02eda3c4a213c98f8
safari <rsafari.s@gmail.com>
2019-05-25 23:41:23 -0500

    code clean up

--- api/bin/smine.js
+++ api/bin/smine.js
@@ -1,15 +1,4 @@
 #!/usr/bin/env node
 
-const smine = require('./../lib/smine')
-
-// const argv = require('yargs')
-//   .option('config', {
-//     alias: 'c',
-//     describe: 'path to config file'
-//   })
-//   .demandOption(['config'])
-//   .version()
-//   .help()
-//   .argv
-
-smine()
+const app = require('../lib/app');
+app();
