9abdb0f235980761b1f183f09faacd83e96488fd
safari <rsafari.s@gmail.com>
2019-06-25 22:53:18 -0500

    mocked up rig devices page

--- ui/src/app/rig/details/rig.details.component.ts
+++ ui/src/app/rig/details/rig.details.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import * as _ from 'lodash/fp';
 
 @Component({
   selector: 'rig-details',
   templateUrl: './rig.details.component.html',
   styleUrls: ['./rig.details.component.css']
 })
-export class RigDetailsComponent {
+export class RigDetailsComponent implements OnInit {
+  gpuCards = [];
+
   constructor(private location: Location) {}
+
+  ngOnInit() {
+    this.gpuCards = _.map(
+      i => ({
+        index: i,
+        name: 'GeForce GTX 1070 ti',
+        temperature: 77,
+        fanSpeed: 23,
+        volatile: 50,
+        power: {
+          usage: 10,
+          cap: 120
+        },
+        memory: {
+          usage: 1000,
+          cap: 8000
+        }
+      }),
+      _.range(0, 6)
+    );
+
+    console.log(this.gpuCards);
+  }
 
   back() {
     this.location.back();
